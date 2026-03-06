d1de155e9b9716dbb3def736ea4ec2c6f826a20c
Uliana Blinova <ulanablinova0@gmail.com>
2026-03-06 20:12:37 +0700

    add component 'cards'

--- js/index.js
+++ js/index.js
@@ -24,9 +24,55 @@ let app = {
 
 function ruleBlock() {
     let colum_2 = app.columns[1];
-    if (colum_2 && colum_2.cards.length >= colum.limit) {
+    if (colum_2 && colum_2.cards.length >= colum_2.limit) {
         app.colum_1_block = true;
     } else {
         app.colum_1_block = false;
     }
 }
+Vue.component('cards', {
+    props: {
+        card: {
+            type: Object,
+            required: true
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        }
+    },
+    template: `
+        <div class="card">
+            <h3>{{ card.title }}</h3>
+            <ul>
+                <li v-for="(item, index) in card.items" :key="index">
+                    <label>
+                        <input 
+                            type="checkbox" 
+                            v-model="item.done"
+                            @change="$emit('update-progress', card)"
+                            :disabled="disabled"
+                        >
+                        <span :class="{ 'text-done': item.done }">
+                            {{ item.text }}
+                        </span>
+                    </label>
+                </li>
+            </ul>
+            <p v-if="card.completedAt" class="completed-date">
+                Завершено: {{ card.completedAt }}
+            </p>
+        </div>
+    `,
+
+})
+new Vue({
+    el: '#app',
+    data: app,
+    methods: {
+        ruleBlock
+    },
+    mounted() {
+        this.ruleBlock();
+    }
+});
