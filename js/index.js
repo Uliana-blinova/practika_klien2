92a984390c1e3af08a40826a05c661f467568321
Uliana Blinova <ulanablinova0@gmail.com>
2026-03-06 20:33:01 +0700

    add component 'colum'

--- js/index.js
+++ js/index.js
@@ -66,6 +66,37 @@ Vue.component('cards', {
     `,
 
 })
+
+Vue.component('colum', {
+    props: {
+        title: String,
+        limit: Number,
+        cards: Array,
+        blocked: Boolean,
+    },
+    template: `
+    <div class="column" :class="{ blocked: isBlocked }">
+            <h2>{{ title }} <span v-if="limit">({{ cards.length }}/{{ limit }})</span></h2>
+            <div class="cards-container">
+                <note-card 
+                    v-for="card in cards" 
+                    :key="card.id" 
+                    :card="card"
+                    :disabled="isBlocked"
+                    @update-progress="hanUpdate"
+                />
+            </div>
+            <div v-if="isBlocked" class="block-overlay">
+                Столбец заблокирован
+            </div>
+        </div>
+`,
+    methods: {
+        hanUpdate(card) {
+            this.$emit('update', card);
+        }
+    }
+})
 new Vue({
     el: '#app',
     data: app,
